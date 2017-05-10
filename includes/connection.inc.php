6354bc95d83078fbe57b167900a810af607eb25b
Zach Walsh <ztwalsh@gmail.com>
2017-05-10 10:14:44 -0500

    Update to bd creds

--- includes/connection.inc.php
+++ includes/connection.inc.php
@@ -1,12 +1,12 @@
 <?php
 // 1. Create a database connection
-$connection = mysql_connect('localhost', 'wallytes_admin', 'Ztwrz841');
+$connection = mysql_connect('localhost', 'ztwalshdb', 'Z#twrz843');
 if (!$connection) {
 	die("Database connection failed: ".mysql_error());
 }
 
 // 2. Select Database to use
-$db_select = mysql_select_db('wallytes_bleucheese', $connection);
+$db_select = mysql_select_db('bleucheeseproductions', $connection);
 if (!$db_select) {
 	die("Database selection failed: ".mysql_error());
 }
